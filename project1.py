75c4a5e340798123a08d8240889e4dd743039fcd
AnteoSanna <52327522+AnteoSanna@users.noreply.github.com>
2019-12-26 21:32:02 -0500

    add more import

---
+++ project1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+dates = pd.date_range('20130101',periods=6)
+
+dates
